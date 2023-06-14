44ac6fe018f99444292be6ce5d5d6b1bd4140f09
Fardeen2001 <1si20et403@sit.ac.in>
2023-06-14 19:43:03 +0530

    book appointment app using axios

---
+++ main.js
@@ -0,0 +1,49 @@
+let form = document.getElementById("my-form");
+let items = document.getElementById("itemsul");
+let details = [];
+
+form.addEventListener('submit', addUsers);
+
+axios.get("https://crudcrud.com/api/b719015445ee4735998344ad9fae424e/appointmentDetails")
+    .then(response => {
+        details = response.data;
+        displayInfo();
+    })
+    .catch(err => {
+        //console.log("Something went wrong:", err);
+        document.body.innerHTML="<h4>Something went wrong</h4>"
+    });
+
+function displayInfo() {
+    items.innerHTML = "";
+    details.forEach(user => {
+        let li = document.createElement('li');
+        li.className = "item";
+        li.innerHTML = `${user.name}-${user.phno}-${user.email}<button class="editbtn" id="edit">Edit</button><button class="delbtn" id="del">Delete</button>`;
+        items.appendChild(li);
+    });
+}
+
+function addUsers(e) {
+    e.preventDefault();
+    let username = document.getElementById("name").value;
+    let userphno = document.getElementById("phno").value;
+    let useremail = document.getElementById("email").value;
+
+    let userDetails = {
+        "name": username,
+        "phno": userphno,
+        "email": useremail
+    };
+
+    axios.post("https://crudcrud.com/api/b719015445ee4735998344ad9fae424e/appointmentDetails", userDetails)
+        .then(response => {
+            details.push(userDetails);
+            displayInfo();
+            form.reset();
+        })
+        .catch(err => {
+            //console.log("Something went wrong:", err);
+             document.body.innerHTML="<h4>Something went wrong</h4>"
+        });
+}
