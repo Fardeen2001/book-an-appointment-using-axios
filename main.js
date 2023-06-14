115771aaf87374b84fb7ff609fdb3af432b577d6
Fardeen2001 <1si20et403@sit.ac.in>
2023-06-14 23:35:33 +0530

    added delete function using axios

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ let details = [];
 
 form.addEventListener('submit', addUsers);
 
-axios.get("https://crudcrud.com/api/b719015445ee4735998344ad9fae424e/appointmentDetails")
+axios.get("https://crudcrud.com/api/3d077a42c1a94a2c8ff09363efddb103/appointmentDetails")
     .then(response => {
         details = response.data;
-        //console.log(details)
         displayInfo();
     })
     .catch(err => {
-        //console.log("Something went wrong:", err);
-        document.body.innerHTML="<h4>Something went wrong</h4>"
+        console.log("Something went wrong:", err);
+        // document.body.innerHTML="<h4>Something went wrong</h4>"
     });
 
 function displayInfo() {
@@ -20,7 +19,7 @@ function displayInfo() {
     details.forEach(user => {
         let li = document.createElement('li');
         li.className = "item";
-        li.innerHTML = `${user.name}-${user.phno}-${user.email}<button class="editbtn" id="edit">Edit</button><button class="delbtn" id="del">Delete</button>`;
+        li.innerHTML = `${user.name}-${user.phno}-${user.email}<button class="editbtn" id="edit">Edit</button><button class="delbtn" id="del" onclick="deleteUser('${user._id}')">Delete</button>`;
         items.appendChild(li);
     });
 }
@@ -37,11 +36,29 @@ function addUsers(e) {
         "email": useremail
     };
 
-    axios.post("https://crudcrud.com/api/b719015445ee4735998344ad9fae424e/appointmentDetails", userDetails)
+    axios.post("https://crudcrud.com/api/3d077a42c1a94a2c8ff09363efddb103/appointmentDetails", userDetails)
         .then(response => {
+            userDetails._id = response.data._id;
             details.push(userDetails);
             displayInfo();
             form.reset();
+        })
+        .catch(err => {
+            console.log("Something went wrong:", err);
+             document.body.innerHTML="<h4>Something went wrong</h4>"
+        });
+}
+
+function deleteUser(userId) {
+    axios.delete(`https://crudcrud.com/api/3d077a42c1a94a2c8ff09363efddb103/appointmentDetails/${userId}`)
+        .then(response => {
+            for (let i = 0; i < details.length; i++) {
+                if (details[i]._id === userId) {
+                  details.splice(i, 1);
+                  break;
+                }
+              }
+            displayInfo();
         })
         .catch(err => {
             //console.log("Something went wrong:", err);
